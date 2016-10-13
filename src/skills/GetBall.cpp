1dce71aba068a5d22f75e430b46a71261cc5f5f9
jim3101 <jimhoekstra2@gmail.com>
2016-10-13 15:11:24 +0200

    relocated a few skills to the skills folder

--- src/skills/GetBall.cpp
+++ src/skills/GetBall.cpp
@@ -84,30 +84,30 @@ public:
 
 } // rtt
 
-bool success;
+// bool success;
 
-void msgCallBackTestSkill(const roboteam_msgs::WorldConstPtr& world, rtt::TestSkill* testSkill) {
-	rtt::LastWorld::set(*world);
-	if (testSkill->Update() == bt::Node::Status::Success) {
-		success = true;
-	}
-}
+// void msgCallBackTestSkill(const roboteam_msgs::WorldConstPtr& world, rtt::TestSkill* testSkill) {
+// 	rtt::LastWorld::set(*world);
+// 	if (testSkill->Update() == bt::Node::Status::Success) {
+// 		success = true;
+// 	}
+// }
 
-int main(int argc, char **argv) {
-	ros::init(argc, argv, "TestSkill");
-	ros::NodeHandle n;
-	rtt::TestSkill testSkill(n);
-	ros::Subscriber sub = n.subscribe<roboteam_msgs::World> ("world_state", 1000, boost::bind(&msgCallBackTestSkill, _1, &testSkill));
-	// ros::Subscriber sub = n.subscribe("world_state", 1000, msgCallBackTestSkill);
-	// <roboteam_msgs::World>
+// int main(int argc, char **argv) {
+// 	ros::init(argc, argv, "TestSkill");
+// 	ros::NodeHandle n;
+// 	rtt::TestSkill testSkill(n);
+// 	ros::Subscriber sub = n.subscribe<roboteam_msgs::World> ("world_state", 1000, boost::bind(&msgCallBackTestSkill, _1, &testSkill));
+// 	// ros::Subscriber sub = n.subscribe("world_state", 1000, msgCallBackTestSkill);
+// 	// <roboteam_msgs::World>
 
-	while (ros::ok()) {
-		ros::spinOnce();
-		if (success) {
-			break;
-		}
-	}
-	ROS_INFO("Skill completed");
+// 	while (ros::ok()) {
+// 		ros::spinOnce();
+// 		if (success) {
+// 			break;
+// 		}
+// 	}
+// 	ROS_INFO("Skill completed");
 
-	return 0;
-}
+// 	return 0;
+// }
