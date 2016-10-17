64a5c719aad739e89379475b135e5b1ade6ee754
jim3101 <jimhoekstra2@gmail.com>
2016-10-17 16:15:01 +0200

    removed commented code for Bob

--- include/roboteam_tactics/skills/GoToPos.h
+++ include/roboteam_tactics/skills/GoToPos.h
@@ -15,33 +15,12 @@ namespace rtt {
 
 class GoToPos : public Skill {
 public:
-// <<<<<<< HEAD
-    // GoToPos();
-    // void Initialize(ros::NodeHandle nh, int robotIDInput);
-    // void UpdateArgs(double xGoalInput, double yGoalInput, double wGoalInput, bool endPoint);
-// =======
     GoToPos(ros::NodeHandle n, std::string name = "", bt::Blackboard::Ptr blackboard = nullptr);
-    // void Initialize(int robotIDInput);
-    // void UpdateArgs(double xGoalInput, double yGoalInput, double wGoalInput);
-// >>>>>>> 44e6355aef817a671ff707bcc69bb740f3c68cc8
 	Status Update();
 private:
 	roboteam_msgs::World prevWorld;
     ros::NodeHandle n;
 	ros::Publisher pub;
-// <<<<<<< HEAD
-	// double xGoal;
-	// double yGoal;
-	// double wGoal;
-	// bool endPoint;
-	// int robotID;
-// =======
-	// double xGoal;
-	// double yGoal;
-	// double wGoal;
-	// int robotID;
-// >>>>>>> 44e6355aef817a671ff707bcc69bb740f3c68cc8
-	
 } ;
 
 } // rtt
