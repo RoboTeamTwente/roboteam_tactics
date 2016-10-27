8f13a4833af5d4fa5395f7a7444103f36a0419cf
bobismijnnaam <bobrubbens@gmail.com>
2016-10-27 16:21:26 +0200

    Added geometry support to tactics.
    Lastworld now has a member to save the current field configuration in. An example of how this is updated can be seen in main.cpp.

--- include/roboteam_tactics/LastWorld.h
+++ include/roboteam_tactics/LastWorld.h
@@ -2,6 +2,7 @@
 
 #include "roboteam_msgs/World.h"
 #include "roboteam_msgs/WorldBall.h"
+#include "roboteam_msgs/GeometryFieldSize.h"
 
 namespace rtt {
 
@@ -10,6 +11,9 @@ class LastWorld {
     static roboteam_msgs::World get();
     static void set(roboteam_msgs::World world);
 
+    static roboteam_msgs::GeometryFieldSize get_field();
+    static void set_field(roboteam_msgs::GeometryFieldSize field);
+
     static roboteam_msgs::Vector2f PredictBallPos(double t);
 
     static bool we_are_blue();
@@ -17,6 +21,7 @@ class LastWorld {
 
     private:
     static roboteam_msgs::World lastWorld;
+    static roboteam_msgs::GeometryFieldSize field;
     static bool blue;
 };
 
