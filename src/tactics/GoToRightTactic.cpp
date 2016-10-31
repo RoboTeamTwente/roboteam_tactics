5acd1511bd7f3e899b5a755e65f4e988bc270efc
bobismijnnaam <bobrubbens@gmail.com>
2016-10-31 17:37:59 +0100

    Continued working on the tactics demo

--- src/tactics/GoToRightTactic.cpp
+++ src/tactics/GoToRightTactic.cpp
@@ -1,6 +1,8 @@
 #include <memory>
 #include <iostream>
 
+#include "roboteam_msgs/Int32Entry.h"
+#include "roboteam_msgs/Float64Entry.h"
 #include "roboteam_msgs/RoleDirective.h"
 #include "roboteam_tactics/tactics/GoToRightTactic.h"
 #include "roboteam_tactics/utils.h"
@@ -15,12 +17,32 @@ GoToRightTactic::GoToRightTactic(bt::Blackboard::Ptr blackboard)
 void GoToRightTactic::Initialize() {
     std::cout << "Initializing GoToRight...\n";
 
+    // Empty directive is reset all!
     roboteam_msgs::RoleDirective resetDirective;
     directivePub.publish(resetDirective);
 
     auto workers = getNodesSubscribedTo("/role_directive");
+    int i = -3;
     for (auto worker : workers) {
         roboteam_msgs::RoleDirective wd;
+        wd.node_id = worker;
+        wd.tree = "GoToPosTree";
+
+        roboteam_msgs::Int32Entry ie;
+        ie.name = "ROBOT_ID";
+        ie.value = 0;
+        wd.blackboard.ints.push_back(ie);
+    
+        roboteam_msgs::Float64Entry fe;
+        fe.name = "xGoal";
+        fe.value = 3;
+        wd.blackboard.doubles.push_back(fe);
+
+        fe.name = "yGoal";
+        fe.value = (double) i;
+        wd.blackboard.doubles.push_back(fe);
+
+        directivePub.publish(wd);
     }
 }
 
