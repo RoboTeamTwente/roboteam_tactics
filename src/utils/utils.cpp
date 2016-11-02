98fee761f41ba595df65d30ad2171678794a6e63
DennisdeW <dennisdw@outlook.com>
2016-11-02 13:58:22 +0100

    add getBallHolder to utils

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -1,9 +1,8 @@
-#include <string>
-#include <vector>
-
+#include "roboteam_tactics/utils/utils.h"
 #include <ros/ros.h>
-
-#include "roboteam_tactics/utils.h"
+#include "roboteam_tactics/LastWorld.h"
+#include "roboteam_msgs/World.h"
+#include "roboteam_tactics/conditions/IHaveBall.h"
 
 namespace rtt {
 
@@ -40,6 +39,27 @@ std::vector<std::string> getNodesSubscribedTo(std::string topic) {
     }
 
     return nodes;
+}
+
+boost::optional<std::pair<roboteam_msgs::WorldRobot, bool>> getBallHolder() {
+    bt::Blackboard::Ptr bb = std::make_shared<bt::Blackboard>();
+    bb->SetBool("our_team", true);
+    for (auto& bot : LastWorld::get().us) {
+        bb->SetInt("me", bot.id);
+        IHaveBall ihb("", bb);
+        if (ihb.Update() == bt::Node::Status::Success) {
+            return boost::optional<std::pair<roboteam_msgs::WorldRobot, bool>>(std::make_pair(bot, true));
+        }
+    }
+    bb->SetBool("our_team", false);
+    for (auto& bot : LastWorld::get().them) {
+        bb->SetInt("me", bot.id);
+        IHaveBall ihb("", bb);
+        if (ihb.Update() == bt::Node::Status::Success) {
+            return boost::optional<std::pair<roboteam_msgs::WorldRobot, bool>>(std::make_pair(bot, false));
+        }
+    }
+    return boost::optional<std::pair<roboteam_msgs::WorldRobot, bool>>();
 }
 
 } // rtt
