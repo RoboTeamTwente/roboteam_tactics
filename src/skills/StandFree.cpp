4e595e3d419f9392b011116395daba840e7e44c3
jim3101 <jimhoekstra2@gmail.com>
2016-11-11 13:43:17 +0100

    update for StandFree

--- src/skills/StandFree.cpp
+++ src/skills/StandFree.cpp
@@ -54,55 +54,87 @@ bt::Node::Status StandFree::Update() {
     	// ROS_INFO("Can't see him...");
     }
 
-    std::vector<roboteam_msgs::WorldRobot> robotsInTheWay = getObstacles(myRobot, theirPos, &world, false);
-    for (size_t i = 0; i < robotsInTheWay.size(); i++) {
-    	if (robotsInTheWay.at(i).id == theirID) {
-    		robotsInTheWay.erase(robotsInTheWay.begin()+i);
-    	}
-    }
+    // std::vector<roboteam_msgs::WorldRobot> robotsInTheWay = getObstacles(myRobot, theirPos, &world, false);
+    // for (size_t i = 0; i < robotsInTheWay.size(); i++) {
+    // 	if (robotsInTheWay.at(i).id == theirID) {
+    // 		robotsInTheWay.erase(robotsInTheWay.begin()+i);
+    // 	}
+    // }
 
+    std::vector<roboteam_utils::Vector2> robotsInTheWay;
     for (size_t i = 0; i < (world.us.size()+world.them.size()); i++) {
     	if (i < world.us.size()) {
-    		if ((world.us.at(i).pos - theirPos).length < (myPos-theirPos).length()) {
-    			roboteam_utils::Vector2 robotPos = roboteam_utils::Vector2(world.us.at(i).pos.x, world.us.at(i).pos.y);
-    		}
+            if (!(GetString("whichTeam") == "us" && theirID == i)) {
+                roboteam_utils::Vector2 robotPos = roboteam_utils::Vector2(world.us.at(i).pos.x, world.us.at(i).pos.y);
+                if ((robotPos - theirPos).length() < (myPos-theirPos).length()) {
+                    robotsInTheWay.push_back(robotPos);
+                }
+            }
     	} else {
-    		int j = i-world.us.size();
-    		if ((world.them.at(j).pos - theirPos).length < (myPos-theirPos).length()) {
-    			roboteam_utils::Vector2 robotPos = roboteam_utils::Vector2(world.us.at(j).pos.x, world.us.at(j).pos.y);
-    		}
+            int j = i-world.us.size();
+            if (!(GetString("whichTeam") == "them" && theirID == j)) {
+                roboteam_utils::Vector2 robotPos = roboteam_utils::Vector2(world.them.at(j).pos.x, world.them.at(j).pos.y);
+                if ((robotPos - theirPos).length() < (myPos-theirPos).length()) {
+                    robotsInTheWay.push_back(robotPos);
+                }
+            }
     	}
     }
 
+    ROS_INFO_STREAM("size of robotsInTheWay: " << robotsInTheWay.size());
+
     roboteam_utils::Vector2 nearestFreePos = myPos;
-	if (robotsInTheWay.size() == 1) {
-    	roboteam_utils::Vector2 robotInTheWayPos = roboteam_utils::Vector2(robotsInTheWay.at(0).pos.x, robotsInTheWay.at(0).pos.y);
-		Cone cone(theirPos, robotInTheWayPos, distanceFromPoint);
-		if (cone.IsWithinCone(myPos)) {
-			nearestFreePos = cone.ClosestPointOnSide(nearestFreePos);
-		}
-    } else if (robotsInTheWay.size() > 1) {
-    	roboteam_utils::Vector2 robotInTheWayPos = roboteam_utils::Vector2(robotsInTheWay.at(0).pos.x, robotsInTheWay.at(0).pos.y);
-		Cone cone(theirPos, robotInTheWayPos, distanceFromPoint);
-    	int i = 0;
-    	ROS_INFO("starting merge");
-    	while (robotsInTheWay.size() > 0) {
-    		roboteam_utils::Vector2 robotInTheWayPos2 = roboteam_utils::Vector2(robotsInTheWay.at(i).pos.x, robotsInTheWay.at(i).pos.y);
-			Cone cone2(theirPos, robotInTheWayPos2, distanceFromPoint);
-    		if (cone.DoConesOverlap(cone2)) {
-    			cone = cone.MergeCones(cone2);
-    			robotsInTheWay.erase(robotsInTheWay.begin()+i);
-    		} else {
-    			ROS_INFO("nope");
-    		}
-    		i++;
-    		if (i >= robotsInTheWay.size()) {i = 0;}
-    	}
-    	ROS_INFO("merge finished!");
-    	if (cone.IsWithinCone(myPos)) {
-			nearestFreePos = cone.ClosestPointOnSide(nearestFreePos);
-		}
+    for (int i = 0; i < robotsInTheWay.size(); i++) {
+        ROS_INFO_STREAM("i: " << i);
+        Cone cone(theirPos, robotsInTheWay.at(i), distanceFromPoint);
+        if (cone.IsWithinCone(myPos)) {
+            for (int j = 0; j < robotsInTheWay.size(); j++) {
+                ROS_INFO_STREAM("j: " << j);
+                if (i!=j) {
+                    Cone cone2(theirPos, robotsInTheWay.at(j), distanceFromPoint);
+                    if (cone.DoConesOverlap(cone2)) {
+                        cone = cone.MergeCones(cone2);
+                        robotsInTheWay.erase(robotsInTheWay.begin()+j);
+                        ROS_INFO_STREAM("merged " << i << " with " << j);
+                    }
+                }
+            }
+            ROS_INFO_STREAM("done merging");
+            nearestFreePos = cone.ClosestPointOnSide(myPos);
+            break;
+        }
     }
+    
+
+ //    roboteam_utils::Vector2 nearestFreePos = myPos;
+	// if (robotsInTheWay.size() == 1) {
+ //    	roboteam_utils::Vector2 robotInTheWayPos = roboteam_utils::Vector2(robotsInTheWay.at(0).pos.x, robotsInTheWay.at(0).pos.y);
+	// 	Cone cone(theirPos, robotInTheWayPos, distanceFromPoint);
+	// 	if (cone.IsWithinCone(myPos)) {
+	// 		nearestFreePos = cone.ClosestPointOnSide(nearestFreePos);
+	// 	}
+ //    } else if (robotsInTheWay.size() > 1) {
+ //    	roboteam_utils::Vector2 robotInTheWayPos = roboteam_utils::Vector2(robotsInTheWay.at(0).pos.x, robotsInTheWay.at(0).pos.y);
+	// 	Cone cone(theirPos, robotInTheWayPos, distanceFromPoint);
+ //    	int i = 0;
+ //    	ROS_INFO("starting merge");
+ //    	while (robotsInTheWay.size() > 0) {
+ //    		roboteam_utils::Vector2 robotInTheWayPos2 = roboteam_utils::Vector2(robotsInTheWay.at(i).pos.x, robotsInTheWay.at(i).pos.y);
+	// 		Cone cone2(theirPos, robotInTheWayPos2, distanceFromPoint);
+ //    		if (cone.DoConesOverlap(cone2)) {
+ //    			cone = cone.MergeCones(cone2);
+ //    			robotsInTheWay.erase(robotsInTheWay.begin()+i);
+ //    		} else {
+ //    			ROS_INFO("nope");
+ //    		}
+ //    		i++;
+ //    		if (i >= robotsInTheWay.size()) {i = 0;}
+ //    	}
+ //    	ROS_INFO("merge finished!");
+ //    	if (cone.IsWithinCone(myPos)) {
+	// 		nearestFreePos = cone.ClosestPointOnSide(nearestFreePos);
+	// 	}
+ //    }
 
     double angleGoal = (theirPos-myPos).angle();
     private_bb->SetInt("ROBOT_ID", myID);
