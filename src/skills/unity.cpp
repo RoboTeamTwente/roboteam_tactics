fe59bc8d389d8d501f434f5c3f7293e91e998774
bobismijnnaam <bobrubbens@gmail.com>
2017-01-31 09:50:30 +0100

    Removed warnings and made cmake prettier

--- src/skills/unity.cpp
+++ src/skills/unity.cpp
@@ -1,23 +1,69 @@
 #include "AimAt.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "AvoidRobots.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "BackUp.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Block.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Chip.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "DefendGoalarea.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Dribble.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Failer.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "FollowPath.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "GetBall.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "GoToPos.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "KeeperBlock.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Kick.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "NaiveBlockGoal.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "ParamSet.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "RandomDrive.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "ReceiveBall.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "RotateAroundPoint.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Runner.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "SecondaryKeeper.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "ShootAtGoal.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "Sleep.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
 #include "StandFree.cpp"
+#undef RTT_CURRENT_DEBUG_TAG 
+
