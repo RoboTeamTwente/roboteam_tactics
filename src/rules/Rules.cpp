144cb9f2394567816b116879caee07a58a5e6644
DennisdeW <dennisdw@outlook.com>
2017-03-03 11:59:25 +0100

    Initial Rules Implementation

---
+++ src/rules/Rules.cpp
@@ -0,0 +1,21 @@
+#include "roboteam_tactics/rules/Rules.h"
+
+namespace rtt {
+    
+bool RefRule::canMove(const TeamRobot&) const { return true; }
+bool RefRule::canMove(const TeamRobot&, const roboteam_utils::Vector2&) const { return true; }
+bool RefRule::canDribble(const TeamRobot&) const { return true; }
+bool RefRule::canDribble(const TeamRobot&, const roboteam_utils::Vector2&) const { return true; }
+bool RefRule::canKick(const TeamRobot&) const { return true; }
+bool RefRule::canChip(const TeamRobot&) const { return true; }
+bool RefRule::canBeOnOpponentSide(const TeamRobot&) const { return true; }
+double RefRule::maxSpeed(const TeamRobot&) const { return INFINITY; }
+double RefRule::minDistanceToBall(const TeamRobot&) const { return 0.0; }
+
+DEFINE_REF_RULE_IMPL(NormalPlayRule, RefState::NORMAL_PLAY)
+
+const std::map<RefState, const RefRule*> RULES = {
+    {RefState::NORMAL_PLAY, NormalPlayRule::get()}
+};
+
+}
