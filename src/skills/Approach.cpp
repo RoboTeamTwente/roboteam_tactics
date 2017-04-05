38bd876b9b15f5cb1f61d3c95fdc2197de9d5c4a
DennisdeW <dennisdw@outlook.com>
2017-04-05 14:13:22 +0200

    Fix Approach

--- src/skills/Approach.cpp
+++ src/skills/Approach.cpp
@@ -1,32 +1,24 @@
 #include "roboteam_tactics/skills/Approach.h"
-#include <signal.h>
 
 namespace rtt {
 
-Approach::Approach(std::string name, bt::Blackboard::Ptr bb) : Skill(name, blackboard) {}
-
-void trap(int) {
-    ROS_INFO("segfault!");
-}
+Approach::Approach(std::string name, bt::Blackboard::Ptr bb) : Skill(name, bb) {}
 
 bt::Node::Status Approach::Update() {
-    //signal(SIGSEGV, trap);
     if (!gtp) {
         bt::Blackboard::Ptr bb = std::make_shared<bt::Blackboard>();
-        ROS_INFO("merging: %p %p", blackboard.get(), bb.get());
-        //merge_blackboards(bb, blackboard);
-        ROS_INFO("building gtp");
+        merge_blackboards(bb, blackboard);
         gtp = std::make_unique<GoToPos>(name, bb);
-        ROS_INFO("gtp built: %p", gtp.get());
     }
-    signal(SIGSEGV, 0);
     auto gtpStatus = gtp->Update();
     if (gtpStatus != bt::Node::Status::Running) {
+        gtp.reset(nullptr);
         return gtpStatus;
     }
     Vector2 goal(GetDouble("xGoal"), GetDouble("yGoal"));
     double dist = fabs(goal.dist(Vector2(getWorldBot(GetInt("ROBOT_ID"))->pos)));
     bool done = dist < HasDouble("approachDistance") ? GetDouble("approachDistance") : DEFAULT_DISTANCE;
+    if (done) gtp.reset(nullptr);
     return done ? bt::Node::Status::Success : bt::Node::Status::Running;
 }
     
