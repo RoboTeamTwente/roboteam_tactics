568840efd34785cd8a62594b75451ce9f9460c5e
dennisdw@outlook.com <dennisdw@outlook.com>
2017-07-20 13:26:52 +0200

    New approach for KeepPosition: works better when evading multiple obstacles

--- include/roboteam_tactics/skills/KeepPosition.h
+++ include/roboteam_tactics/skills/KeepPosition.h
@@ -12,8 +12,6 @@ public:
 	KeepPosition(std::string name, bt::Blackboard::Ptr bb);
 	Status Update() override;
     constexpr static double MINIMUM_ROBOT_DISTANCE = 0.8;
-    constexpr static double MIN_CLEARANCE_WHEN_RETURNING = 0.6;
-    constexpr static double CLEARANCE_STEP_SIZE = .05;
 private:
 	std::unique_ptr<GoToPos> gtp;
 	bool updateGoalPosition();
