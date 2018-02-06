1c9260725997165ed47dc56435c9a068111663af
Emiel Steerneman <emielsteerneman@gmail.com>
2018-02-06 15:39:14 +0100

    Fixed more conflicts stuff

--- src/conditions/IsBallInDefenseArea.cpp
+++ src/conditions/IsBallInDefenseArea.cpp
@@ -19,20 +19,9 @@ namespace rtt {
 // Tweaked by Jelle to use Jelle's variant of getting distance to defense area, which now returns a double.
 bool isWithinDefenseArea(bool ourDefenseArea, Vector2 point) {
     GeometryFieldSize field = LastWorld::get_field();
-<<<<<<< HEAD
-    Vector2 distToDefenseArea = getDistToDefenseArea(ourDefenseArea, point, 0.0);
-    if (ourDefenseArea) {
-        if (distToDefenseArea.x > 0.0 && point.x >= -field.field_length/2) return true;
-        else return false;
-    } else {
-        if (distToDefenseArea.x < 0.0 && point.x <= field.field_length/2) return true;
-        else return false;
-    }
-=======
     double distToDefenseArea = getDistToDefenseArea2(ourDefenseArea, point);
     if (distToDefenseArea < 0) return true;
     else return false;
->>>>>>> jelleExperimental
 }
 
 bool isWithinDefenseArea(bool ourDefenseArea, Vector2 point, double safetyMargin) {
