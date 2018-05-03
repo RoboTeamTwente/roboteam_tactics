57648b90f972aa6be667ddd3517388d1dab82730
Emiel Steerneman <emielsteerneman@gmail.com>
2018-05-03 15:39:57 +0200

    Updated documentation

--- src/utils/RobotPatternGenerator.cpp
+++ src/utils/RobotPatternGenerator.cpp
@@ -9,21 +9,18 @@
 #include "roboteam_utils/Vector2.h"
 #include "roboteam_tactics/utils/RobotPatternGenerator.h"
 
-
 namespace rtt {
 
 	/**
-	 * Creates a line on which robots are placed. Relative to a centre. Returns coordinates in radians
+	 * Creates a line on which robots are placed. Relative to a centre. Returns cartesian coordinates
 	 *
 	 * @param numRobots - The number of robots that should be in the pattern.
 	 * @param lineWidth - The total width of the line in meters
-	 * @param centre    - The center of the pattern, e.g. our goal / the ball / a dangerous robot
-	 * @param angleToCentre - 0 rad -> to the left of the centre; PI rad -> to the right of the centre
+	 * @param centre    - The centre of the pattern, e.g. our goal / the ball / a dangerous robot
+	 * @param angleToCentre - 0 rad -> to the left of the centre; PI/2 rad -> above the centre;
 	 * @param distanceFromCentre - The perpendicular distance between the line and the centre
-	 * @return A tuple with two float vectors, giving positions in radians relative to the centre.
-	 * 		   The first contains angles towards the centre, the second contains distances from the centre.
+	 * @return A vector of Vector2, with each Vector2 containing a position of a robot
 	 */
-
 	std::vector<Vector2> RobotPatternGenerator::Line(int numRobots, float lineWidth, Vector2 centre, float angleToCentre, float distanceFromCentre){
 
 		std::vector<Vector2> x_y_coords;
@@ -52,6 +49,18 @@ namespace rtt {
 		return x_y_coords;
 	}
 
+	/**
+	 * Creates a circle on which robots are placed. Relative to a centre. Returns cartesian coordinates
+	 *
+	 * @param numRobots - The number of robots that should be in the pattern.
+	 * @param circleAngle - The total angle that the pattern should cover. M_PI -> half of a circle; M_PI/2 -> quarter of a circle;
+	 * @param circleRadius - The radius of the circle
+	 * @param centre    - The centre of the pattern, e.g. our goal / the ball / a dangerous robot
+	 * @param phase - The phase of the circle. Sets which degrees the pattern should cover. circleRadius=M_PI & phase=0.1*M_PI -> covers 0.1*M_PI to 1.1*M_PI
+	 * @param angleToCentre - 0 rad -> to the left of the centre; PI/2 rad -> above the centre
+	 * @param distanceFromCentre - The perpendicular distance between the centre of the circle and the centre
+	 * @return A vector of Vector2, with each Vector2 containing a position of a robot
+	 */
 	std::vector<Vector2> RobotPatternGenerator::Circle(int numRobots, float circleAngle, float circleRadius, Vector2 centre, float phase, float angleToCentre, float distanceFromCentre){
 
 		std::vector<Vector2> x_y_coords;
